a08bec98af45529a04a5818ab96caa0d8bb32364
OhMyGuyCH <101036562+OhMyGuyCH@users.noreply.github.com>
2024-03-21 16:59:07 +0700

    Summary

--- app.js
+++ app.js
@@ -4,12 +4,32 @@ const app = express();
 const port = 3000;
 const morgan = require("morgan");
 const path = require("path");
+const product = require("./data/product.json");
+const productRouter = express.Router();
+const indexRouter = express.Router();
 
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
 
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+//
+productRouter.route("/").get((req, res) => {
+  res.render("product", { product });
+});
+productRouter.route("/:id").get((req, res) => {
+  const id = req.params.id;
+  res.render("pd", {
+    product: product[id],
+  });
+});
+app.use("/product", productRouter);
+
+//
+
 app.get("/", (req, res) => {
-  res.send("Hello born");
+  res.render("index", { username: "guyzaazaza" });
 });
 
 app.listen(port, () => {
