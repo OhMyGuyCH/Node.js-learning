54e1c497d4ae7a7437559f594fe35bcfa8344d97
OhMyGuyCH <101036562+OhMyGuyCH@users.noreply.github.com>
2024-03-21 11:41:52 +0700

    add template

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const debug = require("debug")("app");
 const app = express();
 const port = 3000;
 const morgan = require("morgan");
+const path = require("path");
 
 app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
   res.send("Hello born");
 });
 
 app.listen(port, () => {
-  debug("Listenning on port " + port);
+  debug("Listenning on ports " + port);
 });
